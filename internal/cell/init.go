982190bfb693376d8d3e9d8139f3e0de4360b3a1
Potter Xu <xujingchuan1995@gmail.com>
2024-05-31 20:00:33 +0800

    [cell] factory: refactor cell registration

---
+++ internal/cell/init.go
@@ -0,0 +1,19 @@
+package cell
+
+import (
+	"github.com/potterxu/tsanalyzer/internal/cell/impl/converter"
+	"github.com/potterxu/tsanalyzer/internal/cell/impl/reader"
+	"github.com/potterxu/tsanalyzer/internal/cell/impl/writer"
+)
+
+// register the cell here
+func init() {
+	register(type_reader, reader.FileReaderName, reader.NewFileReader, reader.FileReaderHelpShort, reader.FileReaderHelp)
+	register(type_converter, converter.BytesConverterName, converter.NewBytesConverter, converter.BytesConverterHelpShort, converter.BytesConverterHelp)
+	register(type_writer, writer.FileWriterName, writer.NewFileWriter, writer.FileWriterHelpShort, writer.FileWriterHelp)
+}
+
+func register(t cellType, name string, ctor cell_ctor, short cell_short, help cell_help) {
+	cells[t] = append(cells[t], name)
+	factories[name] = &factory{ctor, short, help}
+}
