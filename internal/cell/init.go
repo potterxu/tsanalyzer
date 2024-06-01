14d71e1610907ce1372e7b430daa4f81e26ac384
Potter Xu <xujingchuan1995@gmail.com>
2024-06-01 15:13:25 +0800

    draft

--- internal/cell/init.go
+++ internal/cell/init.go
@@ -2,6 +2,7 @@ package cell
 
 import (
 	"github.com/potterxu/tsanalyzer/internal/cell/impl/converter"
+	"github.com/potterxu/tsanalyzer/internal/cell/impl/processor"
 	"github.com/potterxu/tsanalyzer/internal/cell/impl/reader"
 	"github.com/potterxu/tsanalyzer/internal/cell/impl/writer"
 )
@@ -11,6 +12,7 @@ func init() {
 	register(type_reader, reader.FileReaderName, reader.NewFileReader, reader.FileReaderHelpShort, reader.FileReaderHelp)
 	register(type_converter, converter.BytesConverterName, converter.NewBytesConverter, converter.BytesConverterHelpShort, converter.BytesConverterHelp)
 	register(type_writer, writer.FileWriterName, writer.NewFileWriter, writer.FileWriterHelpShort, writer.FileWriterHelp)
+	register(type_processor, processor.VbvName, processor.NewVbv, processor.VbvHelpShort, processor.VbvHelp)
 }
 
 func register(t cellType, name string, ctor cell_ctor, short cell_short, help cell_help) {
